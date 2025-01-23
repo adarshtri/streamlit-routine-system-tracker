3030cb6fb5d1d681a8dd435cd990e5ec8769bace
adarshtri <adarsh.trivedi100@gmail.com>
2025-01-22 23:15:26 -0800

    Job report by company graph.

--- src/views/jobs/company_based_job_report.py
+++ src/views/jobs/company_based_job_report.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import altair as alt
 import streamlit as st
 from src.firestore.user import UserDoc
 
@@ -35,19 +37,19 @@ def filter_by_company(jobs, selected_company):
 
     return list(filter(lambda x: x["company_name"] == selected_company, jobs))
 
-def filter_by_creation_time(jobs, year, month = None):
+def filter_by_creation_time(jobs, year):
+
+    if year == all_option:
+        return jobs
 
     time = year
-
-    if month:
-        time = time + "-" + month
 
     return list(filter(lambda x: x["creation_time"].startswith(time), jobs))
 
 def created_count(jobs, selected_company, year):
 
     if selected_company == all_option:
-        return len(jobs)
+        return len(filter_by_creation_time(jobs, year))
 
     filtered_by_company = filter_by_company(jobs, selected_company)
     filtered_by_time = filter_by_creation_time(filtered_by_company, year)
@@ -56,14 +58,98 @@ def created_count(jobs, selected_company, year):
 
 def applied_count(jobs, selected_company, year):
 
+    filtered_by_applied = filter_by_applied(jobs, True)
+    filtered_by_time = filter_by_creation_time(filtered_by_applied, year)
+
     if selected_company == all_option:
-        return len(list(filter(lambda x: x["applied"] == True, jobs)))
+        return len(filtered_by_time)
 
     filtered_by_applied = filter_by_applied(jobs, True)
-    filtered_by_company = filter_by_company(filtered_by_applied, selected_company)
-    filtered_by_time = filter_by_creation_time(filtered_by_company, year)
+    filtered_by_time = filter_by_creation_time(filtered_by_applied, year)
+    filtered_by_company = filter_by_company(filtered_by_time, selected_company)
 
-    return len(list(filter(lambda x: x["company_name"] == selected_company and x["applied"] == True, jobs)))
+
+    return len(filtered_by_company)
+
+def create_company_wise_applied_report(jobs, year):
+    filtered_by_applied = filter_by_applied(jobs, True)
+    filtered_by_time = filter_by_creation_time(filtered_by_applied, year)
+
+    company_wise_count = {}
+
+    for job in filtered_by_time:
+
+        company_name = job["company_name"]
+
+        if company_name not in company_wise_count:
+            company_wise_count[company_name] = 0
+
+        company_wise_count[company_name] += 1
+
+    df = pd.DataFrame(company_wise_count.items(), columns=("Company Name", "Count"))
+
+    st.markdown(f"### Applied jobs by Company for {year}")
+
+    y_min = df['Count'].min() - 1
+    y_max = df['Count'].max() + 1
+
+    chart = alt.Chart(df).mark_line().encode(
+        x="Company Name",
+        y=alt.Y('Count', scale=alt.Scale(domain=[y_min, y_max]))
+    ).interactive()
+
+    st.altair_chart(chart, use_container_width=True, theme="streamlit")
+
+    st.dataframe(df, hide_index=True, use_container_width=True)
+
+
+def create_company_wise_report(jobs, year):
+
+    # tracked DF
+
+    filtered_by_time_tracked = filter_by_creation_time(jobs, year)
+
+    company_wise_count_tracked = {}
+
+    for job in filtered_by_time_tracked:
+
+        company_name = job["company_name"]
+
+        if company_name not in company_wise_count_tracked:
+            company_wise_count_tracked[company_name] = 0
+
+        company_wise_count_tracked[company_name] += 1
+
+    df_tracked = pd.DataFrame(company_wise_count_tracked.items(), columns=("Company Name", "Tracked Count"))
+
+    # applied df
+
+    filtered_by_applied = filter_by_applied(jobs, True)
+    filtered_by_time_applied = filter_by_creation_time(filtered_by_applied, year)
+
+    company_wise_count_applied = {}
+
+    for job in filtered_by_time_applied:
+
+        company_name = job["company_name"]
+
+        if company_name not in company_wise_count_applied:
+            company_wise_count_applied[company_name] = 0
+
+        company_wise_count_applied[company_name] += 1
+
+    df_applied = pd.DataFrame(company_wise_count_applied.items(), columns=("Company Name", "Applied Count"))
+
+    df_merged = pd.merge(df_tracked, df_applied, on='Company Name')
+
+
+
+    st.markdown(f"### Tracking by Company for {year}")
+
+
+    st.line_chart(df_merged, x = "Company Name", y = ["Applied Count", "Tracked Count"], x_label="Company Name", y_label="Tracked vs Applied Count", use_container_width=True)
+
+    st.dataframe(df_merged, hide_index=True, use_container_width=True)
 
 def create_company_based_job_report(user_doc: UserDoc):
 
@@ -79,3 +165,6 @@ def create_company_based_job_report(user_doc: UserDoc):
 
     col1.metric("Tracked", created_count(jobs, selection_company_option, selection_year), border=True)
     col2.metric("Applied", applied_count(jobs, selection_company_option, selection_year), border=True)
+
+    st.divider()
+    create_company_wise_report(jobs, selection_year)
