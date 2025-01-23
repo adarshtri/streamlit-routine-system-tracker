a1010cb5a8cbd0adee99f2f80b2efa63c2e716e7
adarshtri <adarsh.trivedi100@gmail.com>
2025-01-22 18:29:26 -0800

    Clean up show jobs UI.

--- src/views/jobs/show_jobs.py
+++ src/views/jobs/show_jobs.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 from src.firestore.user import UserDoc, create_hash, get_current_datetime_utc
 from src.session.user import UserSession
@@ -18,34 +19,50 @@ def show_jobs(user_doc: UserDoc, user_session: UserSession, applied: bool, key_p
     st.markdown(f"## {top_message}")
     st.divider()
 
+    group_by_company = {}
+
     for job in jobs:
-        key = f"{key_prefix}_{user_doc.username}_{create_hash(job['job_url'])}"
+        company_name = job["company_name"]
+
+        if company_name not in group_by_company:
+            group_by_company[company_name] = []
+
+        group_by_company[company_name].append(job)
+
+    for company_name in group_by_company:
+        key = f"{key_prefix}_{user_doc.username}_{create_hash(company_name)}"
+
         applied_icon = "❌"
-        if job["applied"]:
+        if applied:
             applied_icon = "✅"
-        with st.expander(f"{job['role_name']}: {applied_icon}"):
-            st.write(f"Job URL: {job['job_url']}")
-            st.write(f"Company Name: {job['company_name']}")
-            st.write(f"Role Name: {job['role_name']}")
 
-            if applied:
-                st.write(f"Application Date: {job['applied_at'].split(' ')[0]}")
+        with st.expander(f"{company_name}: {len(group_by_company[company_name])} {applied_icon}"):
+            for job in group_by_company[company_name]:
+                internal_key = f"{key_prefix}_{user_doc.username}_{create_hash(job['job_url'])}_1"
+                with st.container():
+                    st.write(f"Job URL: {job['job_url']}")
+                    st.write(f"Company Name: {job['company_name']}")
+                    st.write(f"Role Name: {job['role_name']}")
 
-            if not applied:
-                if st.button("🗑", key=f"{key}_untrack_job_button"):
-                    user_doc.untrack_job(job)
-                    st.success("Job successfully removed from tracking.")
-                    st.rerun()
+                    if applied:
+                        st.write(f"Application Date: {job['applied_at'].split(' ')[0]}")
 
-                applied_button_text = "Change to Applied"
+                    if not applied:
+                        if st.button("🗑", key=f"{internal_key}_untrack_job_button"):
+                            user_doc.untrack_job(job)
+                            st.success("Job successfully removed from tracking.")
+                            st.rerun()
 
-                if job["applied"]:
-                    applied_button_text = "Change to Unapply"
+                        applied_button_text = "Change to Applied"
 
-                if st.button(applied_button_text, key=f'{key}_toogle_apply_button'):
-                    job["applied"] = not job["applied"]
-                    job["applied_at"] = get_current_datetime_utc()
-                    user_doc.applied_to_job(job)
-                    st.rerun()
+                        if job["applied"]:
+                            applied_button_text = "Change to Unapply"
 
+                        if st.button(applied_button_text, key=f'{internal_key}_toogle_apply_button'):
+                            job["applied"] = not job["applied"]
+                            job["applied_at"] = get_current_datetime_utc()
+                            user_doc.applied_to_job(job)
+                            st.rerun()
+
+                    st.divider()
         st.divider()
