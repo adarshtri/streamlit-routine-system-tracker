4c266f0bd985a27e0ffe73914fccb273b15ffa6a
adarshtri <adarsh.trivedi100@gmail.com>
2025-02-09 18:31:57 -0800

    Update personality builder message.

--- src/views/personality_builder.py
+++ src/views/personality_builder.py
@@ -5,12 +5,12 @@ st.title("Personality Builder")
 st.markdown("* Power of creating a positive echo chamber for yourself.\n * You become what you tell yourself everyday.")
 
 st.write(
-    '''This app is designed to act as a positive echo chamber, 
-    reinforcing empowering thoughts and beliefs that help build confidence and build your personality based on how you want it. 
-    Whether it's a message like 'I'm a great flyer' to combat flying anxiety, or other affirmations, the app creates a space to 
-    regularly remind you of your strengths and positive qualities.
-    By creating an environment of positive reinforcement, it helps shift your mindset 
-    and supports mental well-being in challenging situations.
+    '''A personal echo chamber for positive reinforcement. 
+    This project is about creating a space where the thoughts 
+    I want to internalize—like 'I'm a great flyer'—are echoed back to me. 
+    Just as social echo chambers reinforce beliefs through repetition, 
+    this is a deliberate effort to shape my mindset in a way that supports confidence, 
+    growth, and well-being.
     '''
 )
 
